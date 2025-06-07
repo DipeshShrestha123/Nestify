1e6882d14662f5b55b392f420b6d330e833bcb34
DipeshShrestha123 <dipeshshrestha876@gmail.com>
2025-06-07 17:16:18 +0530

    add

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-// import './SignUp.scss';
+import './Signup.scss';
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/AuthProvider";
 
